2b47cee91e07458888fa0d6f9269519af0fec685
unknown <2451076559@qq.com>
2023-11-15 22:12:41 +0800

    FDR校正

--- R/immnune_auto.R
+++ R/immnune_auto.R
@@ -220,6 +220,28 @@ immnune_auto <- function(exposure_path='D:/immune/csv',outcome_data,output_path=
     tm <- floor(t/60)
     ts <- t-60*tm
     cat('分析结束，用时',tm,'分钟',ts,'秒',sep = '')
+    cat('进行FDR校正...')
+
+
+    row_data <- data.table::fread(output_path)
+    data <- row_data[row_data$method=='Inverse variance weighted'&row_data$nsnp>2]
+    data2 <- data[order(data$pval)]
+
+    for (n in 1:nrow(data2)){
+      if (n==nrow(data2)){
+        data2[n,'ajustP'] <- data2[n,'pval']
+      }else{
+        ajustP <- (data2[n,'pval'])*(nrow(data2)/n)
+        if (ajustP[[1]]>1){
+          ajustP <- 1
+        }
+        data2[n,'ajustP'] <- ajustP
+      }
+    }
+    output_file_path <- paste0(gsub('.csv','',output_path),'_FDR.csv')
+    data.table::fwrite(data2,output_file_path)
+
+    cat('完成')
     rm(list = ls())
   }
 }
